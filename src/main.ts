da2cfed9b172ef3f005c8e54aeaaf9014dc2daf5
Leonard Velásquez <leonard.avp@gmail.com>
2022-11-18 21:37:38 -0400

    Utilizar CORS como el de CSV

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{cors:{
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
     origin:'*'
-  }});
+  })
+
   await app.listen(3000);
 }
 bootstrap();
